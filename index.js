e7595d04b40e4192b7ac8e2d35e8b26136d7a174
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-05-11 03:20:47 +0300

    Task updated

--- index.js
+++ index.js
@@ -9,78 +9,89 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces
+  const heightInMeters = height / 100;
+  //const bmi = weight / (height * height);
+  const bmi = weight / Math.pow (height * 0.01,2);
 
- alert(calculateBMI = weight/((height*0.01*height*0.01)));
+  alert(Math.round(bmi));
+
    // part 2
-  if (calculateBMI<18.5)
+   let healthStatus;
+  if (bmi < 18.5)
   {
-    alert("Underweight");
+    healthStatus = "Underweight";
   }
-  else if (calculateBMI >=18.5 && calculateBMI <= 24.9)
+  else if (bmi >=18.5 && bmi <= 24.9)
   {
-    alert("Healthy Weight");
-  }
-
-  else if (calculateBMI >= 25 && calculateBMI <= 29.9)
-  {
-    alert("OVERweight");
+    healthStatus = "Healthy Weight";
   }
 
-  else if (calculateBMI > 30) 
+  else if (bmi >= 25 && bmi <= 29.9)
   {
-    alert("Above Obesity");
+    healthStatus = "OVERweight";
   }
 
-  
+  else if (bmi > 30) 
+  {
+    healthStatus = "Above Obesity";
+  }
+
+  alert(`Your bmi is ${Math.round(bmi)} and is considerd ${healthStatus}`);
+
   // part 3
-
+ let healthStatus1;
  if (age >= 19 && age <= 24){
-  if (calculateBMI >= 19 && calculateBMI <= 24){
+  if (bmi >= 19 && bmi <= 24){
 
-  alert("Healthy");
+    healthStatus1 = "Healthy";
  }
- else 
-  alert("Not Healthy");
+ else {
+ healthStatus1 = "Not Healthy";
+ }
  }
 
  else if (age >= 25 && age <= 34){
-  if (calculateBMI >= 20 && calculateBMI <= 25){
-  alert("Healthy");
+  if (bmi >= 20 && bmi <= 25){
+    healthStatus1 = "Healthy";
   }
-  else 
-  alert("Not Healthy");
+  else {
+  healthStatus1 = "Not Healthy";
+  }
  }
 
  else if (age >= 35 && age <= 44){
-  if (calculateBMI >= 21 && calculateBMI <= 26){
-    alert("Healthy");
+  if (bmi >= 21 && bmi <= 26){
+    healthStatus1 = "Healthy";
   }
-  else 
-  alert("Not Healthy");
-
+  else {
+  healthStatus1 = "Not Healthy";
+  }
  }
  else if (age >= 45 && age <= 54){
-  if (calculateBMI >= 22 && calculateBMI <= 27){
-    alert("Helthy");
+  if (bmi >= 22 && bmi <= 27){
+    healthStatus1 = "Helthy";
   }
-  else 
-  alert ("Not Helthy");
+  else {
+  healthStatus1 = "Not Helthy";
+ }
  }
  
  else if (age >= 55 && age <= 64){
-  if (calculateBMI >= 23 && calculateBMI <= 28){
-    alert("Helthy");
+  if (bmi >= 23 && bmi <= 28){
+    healthStatus1 = "Helthy";
   }
-  else
-  alert ("Not Helthy")
-
+  else {
+  healthStatus1 = "Not Helthy"
+  }
  }
  else if (age >= 65){
-  if (calculateBMI >= 24 && calculateBMI <= 29){
-    alert("Helthy")
+  if (bmi >= 24 && bmi <= 29){
+    healthStatus1 = "Helthy"
   }
-  else 
-  alert ("Not Helthy")
+  else {
+  healthStatus1 = "Not Helthy"
  }
-
+ }
+ alert(`Your bmi is ${Math.round(bmi)} and is considerd ${healthStatus1}`);
 }
+
